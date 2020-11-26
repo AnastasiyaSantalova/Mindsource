8e7d4982b6a024f9eb1baeba06853fff295d7331
Anastasiia Santalova <anastasiia_santalova@epam.com>
2020-11-26 14:32:35 +0300

    [MS-0002] Add Firebase to the project

--- Mindsource/src/environments/environment.prod.ts
+++ Mindsource/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCiwSYLmPj19rDK4ztPAmj4jzD2SZZ3JjM",
+    authDomain: "mindsource-1f8a1.firebaseapp.com",
+    databaseURL: "https://mindsource-1f8a1.firebaseio.com",
+    projectId: "mindsource-1f8a1",
+    storageBucket: "mindsource-1f8a1.appspot.com",
+    messagingSenderId: "551708206102",
+    appId: "1:551708206102:web:30afb537f7e8b4ca1e8137",
+    measurementId: "G-1G2113QLNR"
+  },
 };
--- Mindsource/src/environments/environment.ts
+++ Mindsource/src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCiwSYLmPj19rDK4ztPAmj4jzD2SZZ3JjM",
+    authDomain: "mindsource-1f8a1.firebaseapp.com",
+    databaseURL: "https://mindsource-1f8a1.firebaseio.com",
+    projectId: "mindsource-1f8a1",
+    storageBucket: "mindsource-1f8a1.appspot.com",
+    messagingSenderId: "551708206102",
+    appId: "1:551708206102:web:30afb537f7e8b4ca1e8137",
+    measurementId: "G-1G2113QLNR"
+  },
 };
 
 /*
